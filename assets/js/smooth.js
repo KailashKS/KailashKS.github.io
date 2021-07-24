9443c760e9d490157d7ca16b1860d66fc2ca2921
Kailash K S <karthikkailash6@gmail.com>
2021-07-24 19:51:59 +0530

    Added Smooth Scroll to div

---
+++ assets/js/smooth.js
@@ -0,0 +1,9 @@
+$(document).on("click",".smoothScroll",function(e){
+	e.preventDefault();
+	var id = $(this).attr("href"),
+		topSpace = 30;
+//alert(id);
+	$('html, body').animate({
+	  scrollTop: $(id).offset().top - topSpace
+	}, 800);
+});
