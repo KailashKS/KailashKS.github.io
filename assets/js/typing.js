d5c950b57e328f9c143118b04c78d9f071269adb
Kailash K S <karthikkailash6@gmail.com>
2021-07-26 01:24:47 +0530

    Added new icons, text, CV

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,5 +1,11 @@
-new TypeIt("#last-name", {
-	strings: "Sekhar",
+// new TypeIt("#last-name", {
+// 	strings: "Sekhar",
+// 	speed: 100,
+// 	waitUntilVisible: true
+//   }).go();
+
+  new TypeIt("#custom-text", {
+	strings: "- Cory House",
 	speed: 100,
 	waitUntilVisible: true
   }).go();
