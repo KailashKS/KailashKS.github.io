1e310e0135fb3d468fae28592fa04e995dd4c262
Kailash K S <karthikkailash6@gmail.com>
2021-07-26 12:11:18 +0530

    copy.js commented out

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,24 +1,24 @@
-var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-if (isMobile) {
-	var clipboard = new ClipboardJS('.copy');
-	document.getElementById("copy-email").onclick = true;
-	document.getElementById("copy-number").onclick = true
+// var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+// if (isMobile) {
+// 	var clipboard = new ClipboardJS('.copy');
+// 	document.getElementById("copy-email").onclick = true;
+// 	document.getElementById("copy-number").onclick = true
 
-} else {
-	var clipboard = new ClipboardJS('.copy');
-	clipboard.on('success', function (e) {
-	});
+// } else {
+// 	var clipboard = new ClipboardJS('.copy');
+// 	clipboard.on('success', function (e) {
+// 	});
 
-	var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-	var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-		var popover = new bootstrap.Popover(popoverTriggerEl)
+// 	var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// 	var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+// 		var popover = new bootstrap.Popover(popoverTriggerEl)
 
 
-		popoverTriggerEl.addEventListener('shown.bs.popover', function () {
-			setTimeout(function () {
-				popover.hide();
-			}, 500);
-		});
+// 		popoverTriggerEl.addEventListener('shown.bs.popover', function () {
+// 			setTimeout(function () {
+// 				popover.hide();
+// 			}, 500);
+// 		});
 
-	});
-}
+// 	});
+// }
