e18c9056ee4aef979ef49ccb8c94b6ddee0041b7
Kailash K S <karthikkailash6@gmail.com>
2021-07-26 21:22:03 +0530

    id fixed

--- assets/js/smooth.js
+++ assets/js/smooth.js
@@ -1,13 +1,13 @@
 let chromeAgent = navigator.userAgent.indexOf("Chrome") > -1;
 
 if (chromeAgent) {
-	$(document).on("click",".smoothScroll",function(e){
-		e.preventDefault();
-		var id = $(this).attr("href"),
-			topSpace = 70;
-	//alert(id);
-		$('html, body').animate({
-		  scrollTop: $(id).offset().top - topSpace
-		}, 800);
-	});
+	// $(document).on("click",".smoothScroll",function(e){
+	// 	e.preventDefault();
+	// 	var id = $(this).attr("href"),
+	// 		topSpace = 70;
+	// //alert(id);
+	// 	$('html, body').animate({
+	// 	  scrollTop: $(id).offset().top - topSpace
+	// 	}, 800);
+	// });
 }
